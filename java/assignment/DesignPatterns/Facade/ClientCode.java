be7f371a0e0c29d2b2c3795505014801a31ded2d
iqbal <iqbal>
2023-06-19 09:29:19 +0530

    used args to get input

--- java/assignment/DesignPatterns/Facade/ClientCode.java
+++ java/assignment/DesignPatterns/Facade/ClientCode.java
@@ -2,7 +2,7 @@ public class ClientCode {
     public static void main(String args[]){
         Hotel hotel;
 
-        String menutype = ClientUtil.takeInput();
+        String menutype = args[0];
         HotelKeeper facade = new HotelKeeper();
 
         hotel = facade.hotelMenu(menutype);
