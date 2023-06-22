1f0a02b3b7ac228e58571cb15cafe061f5be707e
iqmd <mdiqofficial@gmail.com>
2023-06-22 13:13:45 +0530

    changes in prtytype

--- java/assignment/DesignPatterns/Prototype/Test.java
+++ java/assignment/DesignPatterns/Prototype/Test.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Test {
-    public static void main(String args){
+    public static void main(String args[]){
         Vehicle bike = new Bike();
         bike.setWheels(2);
         bike.setSeats(1);
