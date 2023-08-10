8d43579d9bc8db2fb08280c198b47ae883a48e31
Mohammad <mqamz@amazon.com>
2023-08-10 13:53:12 +0530

    changes

--- assignment/GameDevelopement/Main.java
+++ assignment/GameDevelopement/Main.java
@@ -1,11 +1,19 @@
+import java.util.*;
+
 public class Main{
     public static void main(String[] args) {
 
+        List<GameCharacter> characters = new ArrayList<>();
+        List<GameCharacter> characterCopy = new ArrayList<>();
         Archer archer = new Archer();
         archer.setName("Green Arrow");
         archer.setHealth(10);
         archer.setAttackPower(20);
         archer.setArrow(100);
+
+        characters.add(archer);
+
+        GameCharacter character = (Archer) archer.clone();
 
     }
 }
